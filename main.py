4fd81b57c877c1c75494d02729d12db00087f099
LincolnCox29 <just.kcgo@gmail.com>
2026-06-03 00:41:35 +1000

    full rework

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import asyncio
-import random
-import requests
 from dotenv import load_dotenv
 import os
-import math
-import json
 from user import User
 
 from aiogram import Bot, Dispatcher, F
@@ -20,230 +16,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-TAGS_POP = None
-def load_tags_pop():
-    global TAGS_POP
-    print(os.path.abspath("tags_pop.json"))
-    try:
-        with open("tags_pop.json", "r", encoding="utf-8") as file:
-            TAGS_POP = json.load(file)
-    except Exception as e:
-        print("tags JSON loading exception: ", e)
-load_tags_pop()
-
-def score_post(user, tags, pop):
-
-    exploration_mod = random.choices(
-        [True, False],
-        weights=[15, 85]
-    )[0]
-
-    score = 0
-    unknown_tags = 0
-    max_neg = 0
-
-    for tag in tags:
-
-        w = user.like_tags.get(tag, 0)
-
-        score += w
-
-        if tag not in user.like_tags:
-            unknown_tags += 1
-
-        if w < max_neg:
-            max_neg = w
-
-    score /= (len(tags) ** 0.5)
-
-    pop_factor = 1 + 0.15 * math.log10(pop + 10)
-    score *= pop_factor
-
-    tag_count_factor = min(1.0, len(tags) / 25)
-    score *= tag_count_factor
-
-    if exploration_mod:
-        unknown_ratio = unknown_tags / len(tags)
-        score += unknown_ratio * 2
-
-    return score
-
-def pick_top_tag(user, k=10):
-    top = sorted(user.like_tags.items(), key=lambda x: x[1], reverse=True)[:k]
-
-    if not top:
-        return None
-
-    tags, scores = zip(*top)
-
-    weights = [max(0.01, s) for s in scores]
-
-    return random.choices(tags, weights=weights, k=1)[0]
-
-def get_random_post(user):
-
-    for i in range(5):
-
-        query = build_query(user)
-        print("QUERY:", query)
-
-        if len(query.split()) >= 3:
-            pid = random.randint(0, 20)
-        else:
-            pid = random.randint(0, 200)
-
-        params = {
-            "page": "dapi",
-            "s": "post",
-            "q": "index",
-            "json": 1,
-            "limit": 100,
-            "pid": pid,
-            "tags": query,
-            "api_key": os.getenv("R34_API_KEY"),
-            "user_id": os.getenv("R34_USER_ID")
-        }
-
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
-
-        try:
-
-            response = requests.get(
-                "https://api.rule34.xxx/index.php",
-                params=params,
-                headers=headers,
-                timeout=10
-            )
-
-            posts = response.json()
-
-            if not posts:
-                continue
-
-            best_post = None
-            for post in posts:
-                if str(post["id"]) in user.posts_cache:
-                    continue
-
-                tags = post["tags"].split()
-                score = score_post(user, tags, post["score"])
-
-                post["tags_score"] = score
-
-                if best_post == None or score > best_post["tags_score"]:
-                    best_post = post
-            return best_post
-
-        except Exception as e:
-            print(e)
-            continue
-
-    return None
-
-def build_query(user):
-
-    query = []
-
-    positive_tags = {
-        tag: score
-        for tag, score in user.like_tags.items()
-        if score > 0
-    }
-
-    negative_tags = {
-        tag: score
-        for tag, score in user.like_tags.items()
-        if score < 0
-    }
-
-    if positive_tags:
-
-        tags = list(positive_tags.keys())
-
-        k = random.choices(
-            [1, 2],
-            weights=[85, 15]
-        )[0]
-
-        top_tags = sorted(
-            positive_tags.items(),
-            key=lambda x: x[1],
-            reverse=True
-        )[:20]
-
-        weights = [
-            score ** 0.5
-            for _, score in top_tags
-        ]
-
-        selected_tags = random.choices(
-            [tag for tag, _ in top_tags],
-            weights=weights,
-            k=k
-        )
-
-        query.extend(set(selected_tags))
-
-    if negative_tags:
-
-        disliked = sorted(
-            negative_tags.items(),
-            key=lambda x: x[1]
-        )
-
-        query.extend(
-            f"-{tag}"
-            for tag, _ in disliked[:2]
-        )
-
-    if user.config["ai_filter"]:
-
-        query.extend([
-            "-ai_generated",
-            "-stable_diffusion",
-            "-midjourney",
-            "-novelai",
-            "-ia_generated"
-        ])
-
-    return " ".join(query)
-
-def print_top_tags(user, limit=50):
-
-    sorted_tags = sorted(
-        user.like_tags.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-
-    print("\n=== TAG SCORES ===\n")
-
-    for i, (tag, score) in enumerate(sorted_tags[:limit], start=1):
-
-        print(
-            f"{i:02d}. "
-            f"{tag:<35} "
-            f"{score:.2f}"
-        )
-
-    print("\n=== MOST DISLIKED ===\n")
-
-    disliked = sorted(
-        user.like_tags.items(),
-        key=lambda x: x[1]
-    )
-
-    for i, (tag, score) in enumerate(disliked[:limit], start=1):
-
-        print(
-            f"{i:02d}. "
-            f"{tag:<35} "
-            f"{score:.2f}"
-        )
-
-# ===== Main Menu =====
+R34_API_KEY = os.getenv("R34_API_KEY")
+R34_USER_ID = os.getenv("R34_USER_ID")
 
 def main_menu():
     return InlineKeyboardMarkup(
@@ -271,8 +45,6 @@ async def back_to_main(callback: CallbackQuery):
 
     await callback.answer()
 
-# ===== Feed Keyboard =====
-
 def feed_keyboard(post_id, liked=False, disliked=False):
 
     return InlineKeyboardMarkup(
@@ -284,7 +56,7 @@ def feed_keyboard(post_id, liked=False, disliked=False):
                 ),
                 InlineKeyboardButton(
                     text="Next",
-                    callback_data="feed"
+                    callback_data=f"feed:{post_id}"
                 )   
             ],
             [
@@ -301,8 +73,6 @@ def feed_keyboard(post_id, liked=False, disliked=False):
             ]
         ]
     )
-
-# ===== Settings =====
 
 @dp.callback_query(F.data == "turn_AI_filter")
 async def turn_AI_filter(callback: CallbackQuery):
@@ -364,20 +134,25 @@ async def start(message: Message):
 
 # ===== Open Feed =====
 
-@dp.callback_query(F.data == "feed")
+@dp.callback_query(F.data.startswith("feed"))
 async def open_feed(callback: CallbackQuery):
 
     user = User(callback.from_user.id)
 
-    post = get_random_post(user)
+    data = callback.data
+    if ":" in data:
+        post_id = data.split(":")[1]
+
+        post = user.posts_cache.get(post_id)
+
+        if post:
+            user.skip_post(post)
+
+    post = user.next_post()
     if post == None:
         return
-    
-    user.posts_cache[str(post["id"])] = post
 
-    if len(user.posts_cache) > 200:
-        oldest_key = next(iter(user.posts_cache))
-        del user.posts_cache[oldest_key] 
+    user.update_posts_cache(post)
 
     image_url = (
         post.get("sample_url")
@@ -396,8 +171,6 @@ async def open_feed(callback: CallbackQuery):
         caption= f"Score: {post['score']}\nTags: {formatted_tags}",
         reply_markup=feed_keyboard(str(post["id"]))
     )
-
-    user.save_user_data()
 
     await callback.answer()
 
@@ -421,39 +194,12 @@ async def like_post(callback: CallbackQuery):
     )
 
     post = user.posts_cache.get(post_id)
-    tags = post["tags"].split()
+    user.like_post(post)
 
-    for tag in tags:
-        if tag not in user.like_tags:
-            user.like_tags[tag] = 0
-
-        base = 1
-
-        pop = TAGS_POP.get(tag, 1)
-        tag_rarity = 1 / (math.log10(pop + 10) ** 2)
-        tag_rarity = max(0.03, tag_rarity)
-
-        base *= tag_rarity
-
-        if "_" in tag:
-            base *= 1.3
-
-        if "(" in tag:
-            base *= 1.8
-
-        preference = 1 + 0.6 * math.tanh(user.like_tags[tag] / 5)
-
-        factor = base * preference
-
-        user.like_tags[tag] += factor
-
-    user.save_user_data()
-
-    print_top_tags(user)
     await callback.answer("Added to favorites")
 
 @dp.callback_query(F.data.startswith("dislike:"))
-async def dislike_post(callback: CallbackQuery):
+async def dislike_post_ui(callback: CallbackQuery):
 
     user = User(callback.from_user.id)
 
@@ -463,42 +209,15 @@ async def dislike_post(callback: CallbackQuery):
 
     post_id = callback.data.split(":")[2]
 
-    print(post_id)
+    print("disliked: ", post_id)
 
     await callback.message.edit_reply_markup(
         reply_markup=feed_keyboard(post_id, disliked=True)
     )
 
     post = user.posts_cache.get(post_id)
-    tags = post["tags"].split()
+    user.dislike_post(post)
 
-    for tag in tags:
-        if tag not in user.like_tags:
-            user.like_tags[tag] = 0
-
-        base = 1
-
-        pop = TAGS_POP.get(tag, 1)
-        tag_rarity = 1 / (math.log10(pop + 10) ** 2)
-        tag_rarity = max(0.03, tag_rarity)
-
-        base *= tag_rarity
-
-        if "_" in tag:
-            base *= 1.3
-
-        if "(" in tag:
-            base *= 1.8
-
-        preference = 1 + 0.6 * math.tanh(-user.like_tags[tag] / 5)
-
-        factor = base * preference
-
-        user.like_tags[tag] -= factor
-
-    user.save_user_data()
-
-    print_top_tags(user)
     await callback.answer("Less like this")
 
 # ===== Main =====
--- user.py
+++ user.py
@@ -1,5 +1,25 @@
 import json
+import math
+import os
+import random
+import requests
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+R34_API_KEY = os.getenv("R34_API_KEY")
+R34_USER_ID = os.getenv("R34_USER_ID")
+
+TAGS_POP = None
+def load_tags_pop():
+    global TAGS_POP
+    print(os.path.abspath("tags_pop.json"))
+    try:
+        with open("tags_pop.json", "r", encoding="utf-8") as file:
+            TAGS_POP = json.load(file)
+    except Exception as e:
+        print("tags JSON loading exception: ", e)
+load_tags_pop()
 
 class User:
 
@@ -13,7 +33,7 @@ class User:
             self.__create_json()
 
     def __create_json(self):
-        self.like_tags = {}
+        self.reactions = {}
         self.posts_cache = {}
         self.config = {
             "ai_filter": False
@@ -23,13 +43,13 @@ class User:
     def __load_json(self):
         with open(self.json_path, "r", encoding="utf-8") as file:
             data = json.load(file)
-        self.like_tags = data["like_tags"]
+        self.reactions = data["reactions"]
         self.posts_cache = data["posts_cache"]
         self.config = data["config"]
         
     def save_user_data(self):
         data = {
-            "like_tags": self.like_tags,
+            "reactions": self.reactions,
             "posts_cache": self.posts_cache,
             "config": self.config
         }
@@ -40,4 +60,268 @@ class User:
                 ensure_ascii=False,
                 indent=4
             )
+
+    def update_posts_cache(self, post):
+        self.posts_cache[str(post["id"])] = post
+
+        if len(self.posts_cache) > 200:
+            oldest_key = next(iter(self.posts_cache))
+            del self.posts_cache[oldest_key]
+
+        self.save_user_data()
+
+    def __reaction(self, type, post):
+        tags = post["tags"].split()
+
+        self.print_tags_weight(max=-3)
+
+        for tag in tags:
+            if tag not in self.reactions:
+                self.reactions[tag] = {
+                    "dis": 0,
+                    "like": 0,
+                    "skip": 0
+                }
+            self.reactions[tag][type] += 1
+
+        self.save_user_data()
+
+    def dislike_post(self, post):
+        self.__reaction("dis", post)
+
+    def like_post(self, post):
+        self.__reaction("like", post)
+
+    def skip_post(self, post):
+        self.__reaction("skip", post)
+
+    def __tag_weight(self, tag):
+
+        stats = self.reactions.get(tag)
+
+        if not stats:
+            return 0
+
+        likes = stats["like"]
+        dislikes = stats["dis"]
+        skips = stats["skip"]
+
+        seen = likes + dislikes + skips
+
+        if seen == 0:
+            return 0
+
+        preference = (
+            likes
+            - dislikes * 2
+            - skips * 0.15
+        ) / seen
+
+        confidence = min(1.0, seen / 10)
+
+        pop = TAGS_POP.get(tag, 100)
+
+        rarity = 1 / math.log10(pop + 10)
+
+        rarity = max(0.15, rarity)
+
+        weight = preference * confidence * rarity * 20
+
+        return weight
+    
+    def __score_post(self, post):
+
+        is_exploration_mod = True if random.random() < 0.15 else False
+
+        tags = post["tags"].split()
+        post_score = post["score"]
+
+        score = 0
+
+        known = 0
+        unknown = 0
+
+        for tag in tags:
+
+            if tag in self.reactions:
+                known += 1
+            else:
+                unknown += 1
+
+            w = self.__tag_weight(tag)
+
+            score += w
+
+        score /= max(1, len(tags) ** 0.5)
+
+        quality_factor = min(
+            1.5,
+            1 + math.log10(post_score + 10) * 0.15
+        )
+
+        score *= quality_factor
+
+        tag_factor = min(
+            1.0,
+            len(tags) / 20
+        )
+
+        score *= tag_factor
+
+        if random.random() < 0.15:
+            score += unknown * 0.25
+
+        if is_exploration_mod:
+            unknown_ratio = unknown / len(tags)
+            score += unknown_ratio * 2
+
+        return score
+    
+    def __get_best_tags(self, limit=20):
+
+        scored = []
+
+        for tag in self.reactions:
+
+            weight = self.__tag_weight(tag)
+
+            if weight > 0:
+                scored.append((tag, weight))
+
+        scored.sort(
+            key=lambda x: x[1],
+            reverse=True
+        )
+
+        return scored[:limit]
+    
+    def __build_query(self):
+
+        query = []
+
+        best_tags = self.__get_best_tags()
+
+        if best_tags:
+
+            tags = [tag for tag, _ in best_tags]
+
+            weights = [weight for _, weight in best_tags]
+
+            positive_count = random.choices(
+                [1, 2],
+                weights=[80, 20]
+            )[0]
+
+            selected = random.choices(
+                tags,
+                weights=weights,
+                k=positive_count
+            )
+
+            query.extend(set(selected))
+
+        disliked = []
+
+        for tag in self.reactions:
+
+            weight = self.__tag_weight(tag)
+
+            if weight < -10:
+                disliked.append((tag, weight))
+
+        disliked.sort(key=lambda x: x[1])
+
+        query.extend(
+            f"-{tag}"
+            for tag, _ in disliked[:10]
+        )
+
+        if self.config["ai_filter"]:
+            query.extend([
+                "-ai_generated",
+                "-stable_diffusion",
+                "-midjourney",
+                "-novelai",
+                "-ia_generated"
+            ])
+
+        return " ".join(query)
+    
+    def next_post(self):
+
+        for i in range(5):
+
+            query = self.__build_query()
+            print("QUERY:", query)
+
+            pid = random.randint(0, 200)
+
+            params = {
+                "page": "dapi",
+                "s": "post",
+                "q": "index",
+                "json": 1,
+                "limit": 100,
+                "pid": 0,
+                "tags": query,
+                "api_key": R34_API_KEY,
+                "user_id": R34_USER_ID
+            }
+
+            headers = {
+                "User-Agent": "Mozilla/5.0"
+            }
+
+            try:
+
+                response = requests.get(
+                    "https://api.rule34.xxx/index.php",
+                    params=params,
+                    headers=headers,
+                    timeout=10
+                )
+
+                posts = response.json()
+                
+                print("STATUS:", response.status_code)
+                print("TYPE:", type(posts))
+                print("TEXT:", response.text[:500])
+
+                if not isinstance(posts, list):
+                    raise TypeError("Bad API response")
+
+                if not posts:
+                    continue
+
+                best_post = None
+                for post in posts:
+                    if str(post["id"]) in self.posts_cache:
+                        continue
+
+                    score = self.__score_post(post)
+
+                    post["user_score"] = score
+
+                    if best_post == None or score > best_post["user_score"]:
+                        best_post = post
+                return best_post
+
+            except Exception as e:
+                print(e)
+                continue
+
+        return None
+    
+    def print_tags_weight(self, min = -999, max = 999):
+        weights = {}
+
+        for tag in self.reactions.keys():
+            w = self.__tag_weight(tag)
+            if w >= min or w <= max:
+                weights[tag] = w
+
+        sorted_by_val = dict(sorted(weights.items(), key=lambda x: x[1]))
+        
+        for tag in sorted_by_val.keys():
+            print(f"Weight of the \"{tag}\": ", weights[tag])
         
